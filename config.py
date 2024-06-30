2426b709b71b3d87fc2e7b4adaaea0e12e962dbd
CHALCOGEN <166417113+CHALCOGEN645@users.noreply.github.com>
2024-06-30 19:50:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 OWNER_USERNAME = getenv("OWNER_USERNAME", "CHALCOGENXD")
 BOT_USERNAME = getenv("BOT_USERNAME", "PENGUIN_X_MUSIC_BOT")
 BOT_NAME = getenv("BOT_NAME", "˹𝙿𝙴𝙽𝙶𝚄𝙸𝙽 ✘ 𝙼ᴜsɪᴄ˼♪")
-ASSUSERNAME = getenv("ASSUSERNAME", "Snowfall_xc")
+ASSUSERNAME = getenv("ASSUSERNAME", "Penguin_assistant")
 EVALOP = list(map(int, getenv("EVALOP", "6797202080").split()))
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 LOGGER_ID = int(getenv("LOGGER_ID", -1002041048920)
@@ -23,7 +23,7 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 17000))
 # External APIs - Get these from their respective providers
 GPT_API = getenv("GPT_API")
 DEEP_API = getenv("DEEP_API")
-OWNER_ID = int(getenv("OWNER_ID", 7044783841))
+OWNER_ID = int(getenv("OWNER_ID", 6536914119))
 
 # Heroku deployment settings - Refer to Heroku documentation on how to obtain these
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
